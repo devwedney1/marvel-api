291d9c2fd9ed50515ce96644e06456c92edf188a
devwedney1 <devwedney@outlook.com>
2022-07-01 11:55:12 +0100

    i init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import api from './services/api';
+
+import fundo from './img/fundo.jpeg'
+
 import './App.css';
 
-function App() {
+export default function Main() {
+  const [comics, setComics] = useState({});
+  
+  useEffect(() => {
+    async function loadComics() {
+      const response = await api.get(
+        "comics?ts=1&apikey=d1a55fa2eb1f4a2a94c15f68a7e75e18&hash=c587ae9d233b8396648871ce70caeff3"
+      );
+      
+      setComics(response.data.data);
+    }
+    loadComics()
+  }, []); 
+  console.log(comics);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div className="main-container">
+      <div className="container-comics">
+        <img src={fundo} alt="fundo" />
+      </div>
+      <div className="box-input">
+          <ul>
+            <li>
+              <a href="">Home</a>
+              <a href="">Comics</a>
+              <a href="">Marvel History</a>
+            </li>
+          </ul>
+      </div>
+    <div className="box-comics">
+      {comics && comics.results && comics.results.length > 0 && (
+        <ul>
+          {comics.results.map((comic) => (
+            <li key={comic.id}>
+              <img src={`${comic.thumbnail.path}.${comic.thumbnail.extension}`} alt="img" />
+              <footer>
+                <strong>
+                  <p className="word">{comic.title}</p>
+                </strong>
+              </footer>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
+  </div>
   );
 }
 
-export default App;
